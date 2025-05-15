4f4d6ee7e964969cca34f612d994b53d74694088
khushi <khushi21091509@gmail.com>
2025-05-15 23:22:25 +0530

    changed

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.post('/api/check', (req, res) => {
     res.json({ percentage, message });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
